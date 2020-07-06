f30705d1d4c0df5acbecdb9fbb7f9e5180f83aa9
John Cinnamond <jcinnamond@gmail.com>
2020-07-06 18:49:51 +0100

    Configure workspaces and keys for workspace switching

--- my-xmonad/Main.hs
+++ my-xmonad/Main.hs
@@ -1,17 +1,35 @@
 module Main (main) where
 
-import XMonad (xmonad, def, logHook, layoutHook, spawn, terminal)
+import XMonad (xmonad, def, logHook, layoutHook, workspaces, spawn, terminal, windows)
+import qualified XMonad.StackSet as W
 
 import XMonad.Hooks.DynamicLog (dynamicLogString, xmobarPP, xmonadPropLog)
 import XMonad.Hooks.ManageDocks (docks, avoidStruts)
 
 import XMonad.Layout (Tall(..))
 
+import XMonad.Util.EZConfig (additionalKeysP)
+
 main :: IO ()
 main = do
   spawn "~/.xmonad/bin/xmobar"
-  xmonad $ docks $ def {
-    terminal = "termite",
-    logHook = dynamicLogString xmobarPP >>= xmonadPropLog,
-    layoutHook = avoidStruts $ Tall 1 (1/100) (1/2)
-    }
+  xmonad $ docks $ myConfig
+
+myConfig = def { terminal = "termite"
+               , logHook = dynamicLogString xmobarPP >>= xmonadPropLog
+               , layoutHook = avoidStruts $ Tall 1 (1/100) (1/2)
+               , workspaces = myWorkspaces
+               }
+           `additionalKeysP` switchWorkspaceKeys
+
+
+myWorkspaces :: [String]
+myWorkspaces = ["dev", "web", "chat", "x", "y", "config", "me", "music"]
+
+homeRow :: [String]
+homeRow = ["a", "s", "d", "f", "g", "j", "k", "l"]
+
+switchWorkspaceKeys = [(maybeShift ++ "M4-" ++ key, action tag)
+                      | (tag, key) <- zip myWorkspaces homeRow
+                      , (maybeShift, action) <- [("", windows . W.greedyView)
+                                                ,("S-", windows . W.shift)]]
